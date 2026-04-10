1d3d954da5594ba3b0c9ce190dbf82e36a2e52a4
CiciTang090905 <wtang28@jh.edu>
2026-04-09 21:16:43 -0400

    kanban-07: Write mutations for create, updateStatus, and remove

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const list = query({
   args: {},
@@ -6,6 +7,45 @@ export const list = query({
     return await ctx.db.query("tasks").collect();
   },
 });
+
+//mutations are for changing data, they don't return data to clients, so no need to return anything.
+export const create = mutation({
+  args: {
+    title: v.string(),
+    description: v.string(),
+  },// validates these at runtime, so if the client sends a number instead of a string, the mutation fails with a clear error.
+  handler: async (ctx, args) => {
+    await ctx.db.insert("tasks", { //insert new document
+      title: args.title,
+      description: args.description,
+      status: "todo",
+    });
+  },
+});
+
+export const updateStatus = mutation({
+  args: {
+    id: v.id("tasks"), //ensure valid id for tasks table
+    status: v.union(
+      v.literal("todo"),
+      v.literal("in-progress"),
+      v.literal("done"),
+    ),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.id, { status: args.status }); //ctx.db.patch(id, fields), partial update for specified fields
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("tasks"),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
+
 //return a new function that Convex registers as a query.
 //access: Combined with the file name, this becomes api.tasks.list
 //pattern is api.<filename>.<exportName>.
@@ -14,3 +54,6 @@ export const list = query({
 //if data changes, it automatically re-runs query and push results to clients
 //for all functions use query
 
+//query, patch, delete, insert
+
+//mutations are transactional --> atomic transaction
